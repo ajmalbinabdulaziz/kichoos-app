bf62e332e6c429eb102a611ad0d8e7ae32b59967
ajmalbinabdulaziz <ajmalbinabdulaziz@gmail.com>
2022-11-16 00:12:25 +0530

    second commit on main

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import '../styles/globals.css'
 import { SessionProvider,  } from "next-auth/react"
-import Header from '../components/header'
-import Footer from '../components/footer'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
 
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
