770bd42e3030ff5df862612ebd086d0465645436
ajmalbinabdulaziz <ajmalbinabdulaziz@gmail.com>
2022-11-24 21:18:27 +0530

    about page finished

--- components/GalleryBox.js
+++ components/GalleryBox.js
@@ -1,14 +1,35 @@
 import React from 'react'
 import Image from 'next/image';
+import ImageSlider, { Slide } from "react-auto-image-slider";
+
+
 
 
 const GalleryBox = () => {
   return (
-    <div className='border rounded-md w-80 overflow-hidden '>
+    <div className='border rounded-md w-80 h-56 overflow-hidden '>
 
-        <div className='relative m-5 mx-7 w-auto h-48 bg-gray-400'>
-            <Image src="/library.jpg" layout='fill' />
-        </div>
+        <ImageSlider effectDelay={500} autoPlayDelay={2000}>
+
+          <Slide>
+            <div className='relative m-3 w-72 h-48 '>
+              <Image src="/library.jpg" layout='fill' />
+            </div>
+          </Slide>
+
+          <Slide>
+            <div className='relative m-3 w-72 h-48 '>
+              <Image src="/football.jpg" layout='fill' />
+            </div>
+          </Slide>
+
+          <Slide>
+            <div className='relative m-3 w-72 h-48 '>
+              <Image src="/casual.jpg" layout='fill' />
+            </div>
+          </Slide>
+
+        </ImageSlider>
 
   </div>
   )
