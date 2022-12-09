5a909bb08689a1b78a426c8485c4b9d786ba2ce5
ajmalbinabdulaziz <ajmalbinabdulaziz@gmail.com>
2022-12-09 17:37:05 +0530

    fixing google auth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,6 +1,6 @@
 import NextAuth from "next-auth"
 // import FacebookProvider from 'next-auth/providers/facebook'
-// import GoogleProvider from 'next-auth/providers/google'
+import GoogleProvider from 'next-auth/providers/google'
 import GithubProvider from "next-auth/providers/github"
 import RedditProvider from "next-auth/providers/reddit"
 // import EmailProvider from 'next-auth/providers/email'
