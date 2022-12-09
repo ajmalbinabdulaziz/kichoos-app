971086bbd4c905dd69bd265feb48d4d0cc65c24a
ajmalbinabdulaziz <ajmalbinabdulaziz@gmail.com>
2022-12-09 18:22:03 +0530

    testing facebook auth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,25 +1,25 @@
 import NextAuth from "next-auth"
-// import FacebookProvider from 'next-auth/providers/facebook'
+import FacebookProvider from 'next-auth/providers/facebook'
 import GoogleProvider from 'next-auth/providers/google'
-import GithubProvider from "next-auth/providers/github"
-import RedditProvider from "next-auth/providers/reddit"
+// import GithubProvider from "next-auth/providers/github"
+// import RedditProvider from "next-auth/providers/reddit"
 // import EmailProvider from 'next-auth/providers/email'
 
 
 export default NextAuth({
   providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
-    }),
+    // GithubProvider({
+    //   clientId: process.env.GITHUB_ID,
+    //   clientSecret: process.env.GITHUB_SECRET,
+    // }),
     // RedditProvider({
     //   clientId: process.env.REDDIT_CLIENT_ID,
     //   clientSecret: process.env.REDDIT_CLIENT_SECRET
     // }),
-    // FacebookProvider({
-    //   clientId: process.env.FACEBOOK_ID,
-    //   clientSecret: process.env.FACEBOOK_SECRET
-    // }),
+    FacebookProvider({
+      clientId: process.env.FACEBOOK_ID,
+      clientSecret: process.env.FACEBOOK_SECRET
+    }),
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET
